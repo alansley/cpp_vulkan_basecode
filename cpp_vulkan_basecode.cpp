eebbc1b8eb33ec131eabf7f980f40f7631f71de0
alansley <aclansley@gmail.com>
2023-05-12 18:25:13 +1000

    End of day push - currently exhibiting extension issues when creating a logical device

--- cpp_vulkan_basecode.cpp
+++ cpp_vulkan_basecode.cpp
@@ -3,12 +3,13 @@
 
 #include <iostream>
 #include <cstdint>
+#include <vector>
 
 using std::cout, std::endl, std::string;
 
 // Vulkan requires an application name and engine name when creating an instance. This is so that AAA games
 // can provide specific details which driver vendors like Nvidia and AMD can then check for to enable or
-// disable specific driver logic. That's it - that's the entire purpose.
+// disable specific driver settings. That's it - that's the entire purpose.
 const string ApplicationName = "cpp_vulkan_basecode";
 const string EngineName = "ACL_vulkan_engine";
 
@@ -18,15 +19,20 @@ const string EngineName = "ACL_vulkan_engine";
 	#include <Windows.h>
 #endif
 
-
-
-
-
 //#include "include/vulkan/vk_platform.h"
 #define VK_NO_PROTOTYPES   // In this example we'll load the functions that we need only, rather than pull in all the prototypes from vulkan.h
-#include <vector>
-
 #include "vulkan/vulkan.h" // Note: "vulkan.h" includes "vk_platform.h" amongst other things
+
+
+// Custom struct for queue details
+struct QueueInfo
+{
+	uint32_t FamilyIndex;
+	std::vector<float> Priorities;
+};
+
+constexpr bool VERBOSE      = true; // Prints out verbose details of what we're doing if true
+constexpr bool VERY_VERBOSE = true; // Prints out additional details of what we're doing if true (e.g., prints all physical device properties, extensions etc.)
 
 // ----- Platform-specific Defines ------
 
@@ -382,9 +388,88 @@ string getFriendlyQueueFlags(VkQueueFlags flagMask)
 	return msg;
 }
 
+/*
+// Method to return a human-readable error message from a VkResult
+// See: https://registry.khronos.org/vulkan/specs/1.3-extensions/man/html/VkResult.html
+string getFriendlyResultString(VkResult result)
+{
+	string msg;
+	switch (result)
+	{
+	// NOT errors..
+	case VK_SUCCESS:                              msg = "VK_SUCCESS";                      break;
+	case VK_NOT_READY:                            msg = "VK_NOT_READY";                    break;
+	case VK_TIMEOUT:                              msg = "VK_TIMEOUT";                      break;
+	case VK_EVENT_SET:                            msg = "VK_EVENT_SET";                    break; 
+	case VK_EVENT_RESET:                          msg = "VK_EVENT_RESET";                  break;
+	case VK_INCOMPLETE:                           msg = "VK_INCOMPLETE";                   break;
+
+	// Errors..
+	case VK_ERROR_OUT_OF_HOST_MEMORY:             msg = "VK_ERROR_OUT_OF_HOST_MEMORY";     break;
+	case VK_ERROR_OUT_OF_DEVICE_MEMORY:           msg = "VK_ERROR_OUT_OF_DEVICE_MEMORY";   break;
+	case VK_ERROR_INITIALIZATION_FAILED:          msg = "VK_ERROR_INITIALIZATION_FAILED";  break;
+	case VK_ERROR_DEVICE_LOST:                    msg = "VK_ERROR_DEVICE_LOST";            break;
+	case VK_ERROR_MEMORY_MAP_FAILED:              msg = "VK_ERROR_MEMORY_MAP_FAILED";      break;
+	case VK_ERROR_LAYER_NOT_PRESENT:              msg = "VK_ERROR_LAYER_NOT_PRESENT";      break;
+	case VK_ERROR_EXTENSION_NOT_PRESENT: break;
+	case VK_ERROR_FEATURE_NOT_PRESENT: break;
+	case VK_ERROR_INCOMPATIBLE_DRIVER: break;
+	case VK_ERROR_TOO_MANY_OBJECTS: break;
+	case VK_ERROR_FORMAT_NOT_SUPPORTED: break;
+	case VK_ERROR_FRAGMENTED_POOL: break;
+	case VK_ERROR_UNKNOWN: break;
+	case VK_ERROR_OUT_OF_POOL_MEMORY:             msg = "VK_ERROR_OUT_OF_POOL_MEMORY or VK_ERROR_OUT_OF_POOL_MEMORY_KHR (same enum value)"; break;
+	case VK_ERROR_INVALID_EXTERNAL_HANDLE:        msg = "VK_ERROR_INVALID_EXTERNAL_HANDLE or VK_ERROR_INVALID_EXTERNAL_HANDLE_KHR (same enum value)";  break;
+	case VK_ERROR_FRAGMENTATION: break;
+	case VK_ERROR_INVALID_OPAQUE_CAPTURE_ADDRESS: break;
+	case VK_PIPELINE_COMPILE_REQUIRED: break;
+	case VK_ERROR_SURFACE_LOST_KHR: break;
+	case VK_ERROR_NATIVE_WINDOW_IN_USE_KHR: break;
+	case VK_SUBOPTIMAL_KHR: break;
+	case VK_ERROR_OUT_OF_DATE_KHR: break;
+	case VK_ERROR_INCOMPATIBLE_DISPLAY_KHR: break;
+	case VK_ERROR_VALIDATION_FAILED_EXT: break;
+	case VK_ERROR_INVALID_SHADER_NV: break;
+	case VK_ERROR_IMAGE_USAGE_NOT_SUPPORTED_KHR: break;
+	case VK_ERROR_VIDEO_PICTURE_LAYOUT_NOT_SUPPORTED_KHR: break;
+	case VK_ERROR_VIDEO_PROFILE_OPERATION_NOT_SUPPORTED_KHR: break;
+	case VK_ERROR_VIDEO_PROFILE_FORMAT_NOT_SUPPORTED_KHR: break;
+	case VK_ERROR_VIDEO_PROFILE_CODEC_NOT_SUPPORTED_KHR: break;
+	case VK_ERROR_VIDEO_STD_VERSION_NOT_SUPPORTED_KHR: break;
+	case VK_ERROR_INVALID_DRM_FORMAT_MODIFIER_PLANE_LAYOUT_EXT: break;
+	case VK_ERROR_NOT_PERMITTED_KHR: break;
+	case VK_ERROR_FULL_SCREEN_EXCLUSIVE_MODE_LOST_EXT: break;
+	case VK_THREAD_IDLE_KHR: break;
+	case VK_THREAD_DONE_KHR: break;
+	case VK_OPERATION_DEFERRED_KHR: break;
+	case VK_OPERATION_NOT_DEFERRED_KHR: break;
+	case VK_ERROR_COMPRESSION_EXHAUSTED_EXT: break;
+	case VK_ERROR_INCOMPATIBLE_SHADER_BINARY_EXT: break;
+
+	// E
+
+	// Duplicate enum values... wtf?!?
+	//case VK_ERROR_OUT_OF_POOL_MEMORY_KHR: break;
+	//case VK_ERROR_INVALID_EXTERNAL_HANDLE_KHR: break;
+	case VK_ERROR_FRAGMENTATION_EXT: break;
+	case VK_ERROR_NOT_PERMITTED_EXT: break;
+	case VK_ERROR_INVALID_DEVICE_ADDRESS_EXT: break;
+	case VK_ERROR_INVALID_OPAQUE_CAPTURE_ADDRESS_KHR: break;
+	case VK_PIPELINE_COMPILE_REQUIRED_EXT: break;
+	case VK_ERROR_PIPELINE_COMPILE_REQUIRED_EXT: break;
+	case VK_RESULT_MAX_ENUM: break;
+
+	default: msg = "Unknown VkResult value: " + result;
+	}
+	
+
+	return msg;
+}
+*/
+
 int main()
 {
-	constexpr bool VERBOSE = true;
+	//constexpr bool VERBOSE = true;
 
 	// Connect to the Vulkan loader library. Note: `LIBRARY_TYPE` is a macro that makes the result a `HMODULE` on Windows and a `void*` on Linux.
 	LIBRARY_TYPE vulkanLibrary;
@@ -451,18 +536,21 @@ int main()
 	}
 
 
-	// Make all the extensions available for use
-	// Note: If we only wanted to make SOME extensions available then we would add them to the desiredExtensions vector directly then check that they ARE available
-	std::vector<char const*> desiredExtensions(instanceExtensionsCount);
-	for (unsigned int i = 0; i < instanceExtensionsCount; ++i) { desiredExtensions[i] = availableExtensions[i].extensionName; }
+	// Ask to make all the extensions we found available for use.
+	// Note: If we only wanted to make SOME extensions available then we would add them to the desiredExtensions vector directly then
+	// check that they ARE available.
+	// Also: We have this as a pointer because later on when we create a `VkCreateDeviceInfo` object we need a pointer-to-a-pointer to the
+	// list of all desired extensions we wish to load for our logical device (it gets passed to `ppEnabledExtensionNames`)!
+	auto pDesiredExtensions = new std::vector<char const*>(instanceExtensionsCount);
+	for (unsigned int i = 0; i < instanceExtensionsCount; ++i) { (*pDesiredExtensions)[i] = availableExtensions[i].extensionName; }
 
 	// Check that all the desired extensions are available. If a desiredExtension was "DoesNotExist" or such then we'll return a failure code.
 	for (unsigned int i = 0; i < instanceExtensionsCount; ++i)
 	{
-		const bool supported = VulkanFunctionLoaders::IsExtensionSupported(availableExtensions, desiredExtensions[i]);
+		const bool supported = VulkanFunctionLoaders::IsExtensionSupported(availableExtensions, (*pDesiredExtensions)[i]);
 		if (!supported)
 		{
-			cout << "[FAIL]: Vulkan extension: " << desiredExtensions[i] << " is not available." << endl;
+			cout << "[FAIL]: Vulkan extension: " << (*pDesiredExtensions)[i] << " is not available." << endl;
 			return -5;
 		}
 	}
@@ -470,6 +558,8 @@ int main()
 
 
 	// Construct the application info required to initialise Vulkan
+	// Note: The application name and engine name properties are used by graphics drivers to twiddle internal driver
+	// settings, typically for specific AAA games.
 	VkApplicationInfo applicationInfo = {
 		VK_STRUCTURE_TYPE_APPLICATION_INFO,
 		nullptr,
@@ -489,8 +579,8 @@ int main()
 		&applicationInfo,
 		0,
 		nullptr,
-		static_cast<uint32_t>(desiredExtensions.size()),
-		!desiredExtensions.empty() ? desiredExtensions.data() : nullptr
+		static_cast<uint32_t>(pDesiredExtensions->size()),
+		!pDesiredExtensions->empty() ? pDesiredExtensions->data() : nullptr
 	};
 
 
@@ -517,7 +607,7 @@ int main()
 
 
 	// Now load the instance-level functions that are provided by our extensions
-	const bool instanceLevelExtensionFunctionsLoaded = VulkanFunctionLoaders::LoadInstanceLevelFunctionFromExtension(vulkanInstance, desiredExtensions);
+	const bool instanceLevelExtensionFunctionsLoaded = VulkanFunctionLoaders::LoadInstanceLevelFunctionFromExtension(vulkanInstance, (*pDesiredExtensions));
 	if (!instanceLevelExtensionFunctionsLoaded)
 	{
 		cout << "[FAIL]: Vulkan instance level functions could not be loaded from extensions." << endl;
@@ -558,7 +648,7 @@ int main()
 	{
 		cout << "[WARNING] Found multiple physical devices - using physical device 0 for now to keep the code manageable." << endl;
 	}
-	auto activePhysicalDevice = availablePhysicalDevices[0];
+	VkPhysicalDevice activePhysicalDevice = availablePhysicalDevices[0];
 
 
 	// Enumerate extension properties on available physical devices.
@@ -582,16 +672,26 @@ int main()
 		return -12;
 	}
 	if (VERBOSE) { cout << "[OK] Populated " << physicalDeviceExtensionCount << " extension properties for physical device 0." << endl; }
-
+	if (VERY_VERBOSE)
+	{
+		cout << "----- Extensions found ------" << endl;
+		for (auto &pdeProperty : physicalDeviceExtensionProperties)
+		{
+			cout << "\t" << pdeProperty.extensionName << " - version: " << pdeProperty.specVersion << endl;
+		}
+	}
+		
 
 	// Get features and properties of physical devices
 	VkPhysicalDeviceFeatures activePhysicalDeviceFeatures;
 	VkPhysicalDeviceProperties activePhysicalDeviceProperties;
 	VulkanFunctionLoaders::vkGetPhysicalDeviceFeatures(activePhysicalDevice, &activePhysicalDeviceFeatures);
 	VulkanFunctionLoaders::vkGetPhysicalDeviceProperties(activePhysicalDevice, &activePhysicalDeviceProperties);
-	printPhysicalDeviceFeatures(activePhysicalDeviceFeatures);
-	printPhysicalDeviceProperties(activePhysicalDeviceProperties);
-
+	if (VERY_VERBOSE)
+	{
+		printPhysicalDeviceFeatures(activePhysicalDeviceFeatures);
+		printPhysicalDeviceProperties(activePhysicalDeviceProperties);
+	}
 
 	// Get details of queue families - in familiar style, we'll do this as a two-step where we first get the number of families and then
 	// populate details of each queue family we found.
@@ -627,29 +727,115 @@ int main()
 
 
 	// Now that we have details of the available queue families, we need to choose one that has our desired capabilities - which in this
-	// case will simply be that we want to draw something.
-	bool foundSuitableQueueFamily = false;
+	// case will simply be that we want to draw something.	
+	int numSuitableFamiliesFound = 0;
 	VkQueueFamilyProperties activeQueueFamily;
 	uint32_t activeQueueFamilyIndex = 0xffffffff; // Set to max possible value initially to avoid "may be uninitialised" moaning
 	VkQueueFlags desiredCapabilities = VK_QUEUE_GRAPHICS_BIT;
 	string desiredCapabilitiesString = getFriendlyQueueFlags(desiredCapabilities);
 	for (int i = 0; i < queueFamiliesCount; ++i)
-	{		
-		if ((queueFamilies[i].queueFlags & desiredCapabilities) != 0)
+	{
+		// If the queue we're looking at has the flag for desired capabilities AND it has a queue available..
+		if ((queueFamilies[i].queueFlags & desiredCapabilities) != 0 && queueFamilies[i].queueCount > 0)
 		{
-			activeQueueFamily        = queueFamilies[i];
-			activeQueueFamilyIndex   = i;
-			foundSuitableQueueFamily = true;
-			break;
+			// ..then if we haven't already found a suitable queue family we have now! Set it! 
+			if (numSuitableFamiliesFound == 0)
+			{
+				activeQueueFamily = queueFamilies[i];
+				activeQueueFamilyIndex = i;
+				++numSuitableFamiliesFound;
+			}
+			// Warn if we found another potential queue family that meets our needs - but to keep things simple for now we'll stick w/ the first one found
+			else if (numSuitableFamiliesFound >= 1)
+			{
+				++numSuitableFamiliesFound;
+				cout << "[WARNING] Found another queue family w/ req'd capabilities, but sticking w/ 1st found. Potential queue families avail now: " << numSuitableFamiliesFound << endl;
+			}
 		}
 	}
-	if (!foundSuitableQueueFamily)
+	if (numSuitableFamiliesFound == 0)
 	{
 		cout << "Could not find a suitable queue family with desired capabilities: " << desiredCapabilitiesString << endl;
 		return -15;
 	}
 	cout << "Found queue family with desired capabilities: " << desiredCapabilitiesString << endl;
-	cout << "Using queue family at index : " << activeQueueFamilyIndex << endl;
+	cout << "Using queue family at index: " << activeQueueFamilyIndex << endl;
 
+
+	// Now we'll create the logical device (on our chosen physical device) which will actually perform our work.
+	QueueInfo activeQueueInfo;
+	activeQueueInfo.FamilyIndex = activeQueueFamilyIndex;
+	std::vector<float> queuePriorities(activeQueueFamily.queueCount, 0.5f); // Fill the vector of queue priorities w/ a value (multiple same values is OK)
+	activeQueueInfo.Priorities = queuePriorities;
+
+	// Create the `DeviceQueueCreateInfo` structure.
+	// CAREFUL: Normally we'd create a vector of `VkDeviceCreateInfo` but because we're using only a single physical device we can use just one here.
+	// CAREFUL: See the Vulkan Cookbook, p51 for further details.
+	VkDeviceQueueCreateInfo deviceQueueCreateInfo {};
+	deviceQueueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO; // Be SUPER careful here - we have to use `DEVICE QUEUE CREATE INFO`!
+	deviceQueueCreateInfo.pNext = nullptr;
+	deviceQueueCreateInfo.flags = 0;
+	deviceQueueCreateInfo.queueFamilyIndex = activeQueueFamilyIndex;
+	deviceQueueCreateInfo.queueFamilyIndex = activeQueueFamily.queueCount;
+	deviceQueueCreateInfo.pQueuePriorities = queuePriorities.data();
+
+	// Create a pointer to a vector strings listing all the desired extensions we wish to create on the logical device
+	// Note: In this instance we'll just add them all, but we could add only individual extensions if we wanted.
+	// TODO: Test out adding only a small number of extensions and then trying to use an extension we DID NOT request to ensure it fails!
+	auto pDesiredExtensionsConstCharVect = new std::vector<char const *>(physicalDeviceExtensionCount);
+	for (int i = 0; i < physicalDeviceExtensionCount; ++i)
+	{
+		(*pDesiredExtensionsConstCharVect)[i] = physicalDeviceExtensionProperties[i].extensionName;
+	}
+	cout << "[OK] Requesting to load: " << pDesiredExtensionsConstCharVect->size() << " extensions." << endl;
+
+	// Create a `DeviceCreateInfo` object - this part is a bit fiddly...
+	VkDeviceCreateInfo deviceCreateInfo {};
+	deviceCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO; // Be SUPER careful here, too - we have to use `DEVICE CREATE INFO`!
+	deviceCreateInfo.pNext = nullptr;
+	deviceCreateInfo.flags = 0;
+	deviceCreateInfo.queueCreateInfoCount = 1; // This is because we're just using a single physical device for the time being!
+	deviceCreateInfo.pQueueCreateInfos = &deviceQueueCreateInfo; // This would normally be a pointer to the start of a vector of `QueueCreateInfo`s!
+	deviceCreateInfo.enabledLayerCount = 0;
+	deviceCreateInfo.ppEnabledLayerNames = nullptr;
+	deviceCreateInfo.enabledExtensionCount = pDesiredExtensionsConstCharVect->size(); // As we may be loading only a subset of all available extensions it prolly makes sense to use that as the count rather than `physicalDeviceExtensionCount` which we obtained earlier
+
+	//std::vector<const char*> ppEnabledExtentionNames;
+	//for (int i = 0; i < pDesiredExtensions.size(); ++i)
+		//ppEnabledExtentionNames.push_back(pDesiredExtensions->at(i)->c_str())
+
+	std::vector<const char*> ptrs;
+	for (std::string const& str : (*pDesiredExtensions)) {
+		ptrs.push_back(str.data());
+	}
+	//deviceCreateInfo.ppEnabledExtensionNames = ptrs.data(); // pDesiredExtensions->data(); // Pointer-to-a-pointer e.g., `const char* const *`
+
+	deviceCreateInfo.ppEnabledExtensionNames = pDesiredExtensions->data(); // Pointer-to-a-pointer e.g., `const char* const *`
+	// I THINK THE ABOVE LINE IS THE ISSUE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+	deviceCreateInfo.pEnabledFeatures = &activePhysicalDeviceFeatures;
+
+	// FINALLY create the logical device!!
+	VkDevice logicalDevice;
+	result = VulkanFunctionLoaders::vkCreateDevice(activePhysicalDevice, &deviceCreateInfo, nullptr, &logicalDevice);
+	if (result != VK_SUCCESS)
+	{
+		cout << "[FAIL] Failed to create logical device. VkResult code is: " << result << endl;
+		return -16;
+	}
+	if (VERBOSE) { cout << "[OK] Successfully created logical device." << endl; }
+
+	
+
+	/*
+	deviceCreateInfo.queueCreateInfoCount = activeQueueFamily.queueCount;
+	deviceCreateInfo.pQueueCreateInfos = activeQueueInfo;
+	deviceCreateInfo.
+	deviceCreateInfo.
+	deviceCreateInfo.
+	*/
+
+
+	
 
 }
